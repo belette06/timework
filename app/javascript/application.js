e1a481cda2b103714fb2422114ee707633452d5b
belette06 <cyril.avronsart@gmail.com>
2022-08-28 13:49:18 +0200

    front

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,3 +3,13 @@ import "@hotwired/turbo-rails"
 import "./controllers"  
 import * as bootstrap from "bootstrap"  
 window.bootstrap = bootstrap
+
+
+
+
+$(document).ready(function(){
+  $(".clickable-link").on('click', function(e){
+    url = $(this).attr('data-link-path');
+    window.location = url;
+  });
+});
